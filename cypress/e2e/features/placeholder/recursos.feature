1f246def9df28b138367b33caa06a701a7e53d02
Rodrigo Diasm <rodrigo.rocha.dias@hotmail.com>
2025-02-25 17:26:54 -0300

    Ajustes nos steps

--- cypress/e2e/features/placeholder/recursos.feature
+++ cypress/e2e/features/placeholder/recursos.feature
@@ -3,7 +3,7 @@ Feature: Placeholder - Recursos
 
     Contexto:
     Este teste verifica se os recursos podem ser consultados e atualizados corretamente utilizando a API JSONPlaceholder.
-  
+
     Cenários testados:
     1. Criar
     2. Consultar por ID
@@ -25,59 +25,38 @@ Feature: Placeholder - Recursos
         | Teste Titulo 6   | Teste Corpo 6 | 1234567891011121314ABC | 201           | Teste Titulo 6 | Teste Corpo 6 | 1234567891011121314ABC |
 
     @regressivo @recursos
-    Scenario: Buscar recurso por ID
-        Given fizer consulta de recurso por id "1"
-        When obter o codigo de resposta "200"
-        Then sera retornado consulta do recurso "1" com sucesso
+    Scenario Outline: Buscar recurso por ID - "<tipo>"
+        Given fizer consulta de recurso por "<tipo>"
+        Then sera retornado consulta do recurso com sucesso
+        Examples:
+        | tipo         |
+        | sucesso      |
+        | invalido     |
+        | null         |
+        | longo        |
+        | inexistente  |
 
-    # Isso pra mim é um bug, o response veio um array sem nenhuma informação
     @regressivo @recursos
-    Scenario: Buscar recurso por ID - Falha (ID inexistente)
-        Given fizer consulta de recurso por id "11111111111111111111111111111111"
-        When obter o codigo de resposta "404"
+    Scenario Outline: Atualizar recurso por ID - "<tipo>"
+        Given que faca a atualizacao do recurso por "<tipo>"
+        Then sera retornado no response os dados atualizados corretamente ou uma mensagem de erro
+        Examples:
+        | tipo         |
+        | sucesso      |
+        | invalido     |
+        | null         |
+        | longo        |
+        | inexistente  |
+
+    # endpoint aparentemente com erro, deveria me retornar status response de erro e devolveu um 200
+    @regressivo @recursos
+    Scenario Outline: Excluir recurso por ID - "<tipo>"
+        Given que faca a exclusao do recurso por "<tipo>"
         # Then sera retornado consulta do recurso com sucesso
-
-    # Isso pra mim é um bug, o response veio um array sem nenhuma informação do id "" que consultei
-    @regressivo @recursos
-    Scenario: Buscar recurso por ID
-        Given fizer consulta de recurso por id ""
-        When obter o codigo de resposta "200"
-        # Then sera retornado consulta do recurso "1" com sucesso
-
-    @regressivo @recursos
-    Scenario: Atualizar recurso por ID
-        Given que faca a atualizacao do recurso por id "1"
-        When obter o codigo de resposta "200"
-        Then sera consultado pelo id "1" e sera atualizado o recurso "1" com sucesso
-
-    @regressivo @recursos
-    Scenario: Atualizar recurso por ID - Falha (ID inexistente)
-        Given que faca a atualizacao do recurso por id "2222"
-        When obter o codigo de resposta "500"
-        Then sera retornado mensagem no response "TypeError: Cannot read properties of undefined"
-
-    @regressivo @recursos
-    Scenario: Atualizar recurso por ID - Falha (ID inválido)
-        Given que faca a atualizacao do recurso por id "11111111111111111111111111111111111111111111111111111111111111"
-        When obter o codigo de resposta "500"
-        Then sera retornado mensagem no response "TypeError: Cannot read properties of undefined"
-
-    @regressivo @recursos
-    Scenario: Excluir recurso por ID
-        Given que faca a exclusao do recurso por id "1"
-        When obter o codigo de resposta "200"
-        Then sera consultado pelo id "1" e sera atualizado o recurso "1" com sucesso
-
-    # endpoint aparentemente com erro, deveria me retornar status response de erro e devolveu um 200 
-    @regressivo @recursos
-    Scenario: Excluir recurso por ID - Falha (ID inexistente)
-        Given que faca a exclusao do recurso por id "2222"
-        When obter o codigo de resposta "200"
-        # Then sera retornado mensagem no response "TypeError: Cannot read properties of undefined"
-
-    # endpoint aparentemente com erro, deveria me retornar status response de erro e devolveu um 200 
-    @regressivo @recursos
-    Scenario: Excluir recurso por ID - Falha (ID inválido)
-        Given que faca a exclusao do recurso por id "11111111111111111111111111111111111111111111111111111111111111"
-        When obter o codigo de resposta "200"
-        # Then sera retornado mensagem no response "TypeError: Cannot read properties of undefined"
+        Examples:
+        | tipo         |
+        | sucesso      |
+        | invalido     |
+        | null         |
+        | longo        |
+        | inexistente  |
--- cypress/support/steps/placeholder/recursos.step.js
+++ cypress/support/steps/placeholder/recursos.step.js
@@ -1,50 +1,104 @@
 /* global Given, Then, When */
 import Recurso from "../../services/placeholder/recurso.api";
 const recurso = new Recurso();
+import { faker } from '@faker-js/faker';
+
+const testCases = {
+    sucesso: { id: 1, statusEsperado: { get: 200, put: 200 } },
+    invalido: { id: 2222, statusEsperado: { get: 404, put: 500 } },
+    null: { id: null, statusEsperado: { get: 404, put: 500 } },
+    longo: { id: "11111111111111111111111111111111111111111111111111111111111111", statusEsperado: { get: 404, put: 500 } },
+    inexistente: { id: "0000", statusEsperado: { get: 404, put: 500 } }
+}
 
 Given('que faca a criacao de um novo recurso titulo {string} corpo {string} e usuarioId {string}', (title, bodyRequest, userId) => {
-    recurso.criarRecurso(title, bodyRequest, userId)
+    const dadosCriacao = {
+        title: title || faker.lorem.words(3),
+        bodyRequest: bodyRequest || faker.lorem.sentence(),
+        userId: userId || faker.number.int({ min: 1, max: 100 })
+    }
+    cy.wrap(dadosCriacao).as('requestData')
+    recurso.criarRecurso(dadosCriacao.title, dadosCriacao.bodyRequest, dadosCriacao.userId)
+        .then((response) => {
+            expect(response.status).to.eq(201)
+            cy.wrap(response).as('responseData')
+        })
 })
 
-Given('fizer consulta de recurso por id {string}', (id) => {
-    recurso.buscarRecurso(id)
+Given('fizer consulta de recurso por {string}', (tipo) => {
+    const { id, statusEsperado } = testCases[tipo] || testCases.sucesso;
+    cy.wrap(id).as('requestId')
+    cy.wrap(statusEsperado.get).as('expectedStatus')
+    recurso.buscarRecurso(id).then((response) => {
+        cy.wrap(response).as('responseData')
+    })
 })
 
-Given('que faca a atualizacao do recurso por id {string}', (id) => {
-    let title = 'Titulo atualizado'
-    let bodyRequest = 'Corpo atualizado'
-    let userId = 2
-    recurso.atualizarRecurso(id, title, bodyRequest, userId)
+Given('que faca a atualizacao do recurso por {string}', (tipo) => {
+    const { id, statusEsperado } = testCases[tipo] || testCases.sucesso
+    const title = faker.lorem.words(3)
+    const bodyRequest = faker.lorem.sentence()
+    const userId = faker.number.int({ min: 1, max: 100 })
+    cy.wrap({ id, title, bodyRequest, userId }).as('requestData')
+    cy.wrap(statusEsperado.put).as('expectedStatus')
+    recurso.atualizarRecurso(id, title, bodyRequest, userId).then((response) => {
+        cy.wrap(response).as('responseData')
+    })
 })
 
-Given('que faca a exclusao do recurso por id {string}', (id) => {
-    recurso.deletarRecurso(id)
+Given('que faca a exclusao do recurso por {string}', (tipo) => {
+    const { id, statusEsperado } = testCases[tipo] || testCases.sucesso;
+    cy.wrap(id).as('requestId')
+    cy.wrap(statusEsperado.get).as('expectedStatus')
+    recurso.deletarRecurso(id).then((response) => {
+        cy.wrap(response).as('responseData')
+    })
 })
 
 Then('sera retornado recurso com titulo {string} corpo {string} e usuarioId {string} com sucesso', (title, bodyRequest, userId) => {
-    cy.get('@last_response').should((response) => {
-        expect(response.body.body).to.eq(bodyRequest)
-        expect(response.body.title).to.eq(title)
-        expect(response.body.userId).to.eq(userId)
-        expect(response.headers).to.have.property('content-type').that.includes('application/json');
+    cy.get('@requestData').then((requestData) => {
+        cy.get('@responseData').should((response) => {
+            expect(response.status).to.eq(201)
+            expect(response.body.title).to.eq(title || requestData.title)
+            expect(response.body.body).to.eq(bodyRequest || requestData.bodyRequest)
+            expect(response.body.userId.toString()).to.eq(userId || requestData.userId.toString())
+            expect(response.headers).to.have.property('content-type').that.includes('application/json')
+        })
     })
 })
 
-Then('sera retornado consulta do recurso {string} com sucesso', (id) => {
-    cy.get('@last_response').should((response) => {
-        expect(response.body.id).to.eq(Number(id))
-        expect(response.headers).to.have.property('content-type').that.includes('application/json');
+Then('sera retornado consulta do recurso com sucesso', () => {
+    cy.get('@requestId').then((id) => {
+        cy.get('@expectedStatus').then((statusEsperado) => {
+            cy.get('@responseData').should((response) => {
+                expect(response.status).to.eq(statusEsperado)
+
+                if (statusEsperado === 200) {
+                    expect(response.body.id).to.eq(Number(id))
+                    expect(response.headers).to.have.property('content-type').that.includes('application/json')
+                } else {
+                    expect(response.body).to.be.empty
+                }
+            })
+        })
     })
 })
 
-Then('sera consultado pelo id {string} e sera atualizado o recurso {string} com sucesso', (idConsulta, idAtualizacap) => {
-    recurso.buscarRecurso(idConsulta)
-    cy.get('@last_response').should((response) => {
-        expect(response.body.id).to.eq(Number(idAtualizacap))
-        // Essa API de teste nao esta atualizando nesse endpoint, mas a validação que eu faria é essa, depois que foi feito o PictureInPictureEvent, deveria trocar no response 
-        // expect(response.body.title).to.eq('Título atualizado')
-        // expect(response.body.body).to.eq('Corpo atualizado')
-        // expect(response.body.userId).to.eq(2)
-        expect(response.headers).to.have.property('content-type').that.includes('application/json')
+Then('sera retornado no response os dados atualizados corretamente ou uma mensagem de erro', () => {
+    cy.get('@expectedStatus').then((statusEsperado) => {
+        cy.get('@responseData').then((response) => {
+            expect(response.status).to.eq(statusEsperado)
+            if (statusEsperado === 200) {
+                cy.get('@requestData').then(({ id, title, bodyRequest, userId }) => {
+                    expect(response.body.id).to.eq(id)
+                    expect(response.body.title).to.eq(title)
+                    expect(response.body.body).to.eq(bodyRequest)
+                    expect(response.body.userId).to.eq(userId)
+                    expect(response.headers).to.have.property('content-type').that.includes('application/json')
+                })
+            } else {
+                expect(response.body).to.contain("TypeError: Cannot read properties of undefined")
+            }
+        })
     })
 })
